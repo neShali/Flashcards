d927acd1e3a8961df8a2694cacd9d7ebdd60651c
abakharev <abakharev@sharevault.com>
2023-01-27 09:27:45 +0300

    oop only

--- mvc/runner.js
+++
@@ -1,9 +0,0 @@
-const Controller = require('./Controller');
-const Model = require('./Model');
-const View = require('./View');
-
-const model = new Model();
-const view = new View();
-const controller = new Controller(model, view);
-
-controller.run();
