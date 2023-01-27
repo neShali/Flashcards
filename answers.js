d067008496ef48420b80c752d093b38da2978d79
Vladimir <st-26region90@mail.ru>
2023-01-27 14:43:11 +0300

    newJs

--- answers.js
+++ answers.js
@@ -1,21 +1,39 @@
-const fs=require('fs');/// promisses
-const argv=process.argv
+const fs = require('fs'); /// promisses
+const { Module } = require('module');
 
+const { argv } = process;
 
-class Controller{
-    constructor(){
+class Answers {
+  constructor(answer) {
+    this.answer = answer;
+  }
 
+  getA(address) {
+    const arr = [];
+
+    //    const tema= fs.readdirSync(`${__dirname}/topics`, 'utf-8')
+    const topics = fs
+      .readFileSync(`${__dirname}/topics/${address}`, 'utf-8')
+      .split('\n');
+    for (let i = 1; i < topics.length; i += 3) {
+      arr.push(topics[i]);
     }
+    this.answer = arr;
+  }
 }
 
+// a.getA('enot.txt')
 
-const tema= fs.readdirSync(`${__dirname}/topics`, 'utf-8')
-console.log(tema[0] )
-const topics= fs.readFileSync(`${__dirname}/topics/${tema[0]}`, 'utf-8').split('\n')
-let arr=[]
-for (let i=1;i<topics.length;i+=3){
-    arr.push(topics[i])
-}
+// console.log(a)
 
-console.log(arr)
-// exports module=Controller
+module.exports = Answers;
+// const tema= fs.readdirSync(`${__dirname}/topics`, 'utf-8')
+// console.log(tema[0] )
+// const topics= fs.readFileSync(`${__dirname}/topics/${tema[0]}`, 'utf-8').split('\n')
+// let arr=[]
+// for (let i=1;i<topics.length;i+=3){
+//     arr.push(topics[i])
+// }
+
+// console.log(arr)
+// exports module=Answers
